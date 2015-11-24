3896669d2c5155e20dd32b2d329d94d92ca99989
shixz <shixz@zhongsou.com>
2015-11-24 18:43:42 +0800

    开发代码提交

---
+++ src/service/service_grpc.cpp
@@ -0,0 +1,120 @@
+#include "service/service_grpc.h"
+#include "glog/logging.h"
+
+namespace rsys {
+  namespace news {
+    ServiceGrpc::ServiceGrpc(CandidateDB* candidate_db): candidate_db_(candidate_db)
+    {
+    }
+
+    ServiceGrpc::~ServiceGrpc()
+    {
+    }
+
+    grpc::Status ServiceGrpc::failed_status_glue(const Status& status)
+    {
+      if (status.isNotFound()) {
+        return grpc::Status(grpc::NOT_FOUND, status.toString());
+      } else if (status.isCorruption()) {
+        return grpc::Status(grpc::INTERNAL, status.toString());
+      } else {
+        return grpc::Status(grpc::UNKNOWN, status.toString());
+      }
+    }
+
+    grpc::Status ServiceGrpc::updateAction(grpc::ServerContext* context, const Action* request, Action* response)
+    {
+      Status status = candidate_db_->updateAction(*request, *response);
+      if (status.ok()) {
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::updateItem(grpc::ServerContext* context, const Item* request, proto::Status* response)
+    {
+      Status status = candidate_db_->addItem(*request);
+      if (status.ok()) {
+        response->set_code(proto::CODE_OK);
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::updateSubscribe(grpc::ServerContext* context, const Subscribe* request, proto::Status* response)
+    {
+      Status status = candidate_db_->updateSubscribe(*request);
+      if (status.ok()) {
+        response->set_code(proto::CODE_OK);
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+      
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::updateFeedback(grpc::ServerContext* context, const Feedback* request, proto::Status* response)
+    {
+      Status status = candidate_db_->updateFeedback(*request);
+      if (status.ok()) {
+        response->set_code(proto::CODE_OK);
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::queryUserStatus(grpc::ServerContext* context, const User* request, UserStatus* response)
+    {
+      Status status = candidate_db_->findUser(*request);
+      if (status.ok()) {
+        response->set_is_new_user(0);
+        return grpc::Status::OK;
+      } else if (status.isNotFound()) {
+        response->set_is_new_user(1);
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::queryCandidateSet(grpc::ServerContext* context, const Recommend* request, CandidateSet* response)
+    {
+      Status status = candidate_db_->queryCandidateSet(*request, *response);
+      if (status.ok()) {
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::queryUserInfo(grpc::ServerContext* context, const proto::UserQuery* request, proto::UserInfo* response)
+    {
+      Status status = candidate_db_->queryUserInfo(*request, *response);
+      if (status.ok()) {
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+
+    grpc::Status ServiceGrpc::queryItemInfo(grpc::ServerContext* context, const proto::ItemQuery* request, proto::ItemInfo* response)
+    {
+      Status status = candidate_db_->queryItemInfo(*request, *response);
+      if (status.ok()) {
+        return grpc::Status::OK;
+      }
+      LOG(ERROR)<<status.toString()<<", from="<<context->peer();
+
+      return failed_status_glue(status);
+    }
+  } // namespace news
+} // namespace rsys
---
+++ src/service/service_grpc.h
@@ -0,0 +1,34 @@
+#ifndef RSYS_NEWS_SERVICE_GRPC_H
+#define RSYS_NEWS_SERVICE_GRPC_H
+
+#include "core/candidate_db.h"
+#include "proto/service.grpc.pb.h"
+
+namespace rsys {
+  namespace news {
+    // 实现GRPC接口
+    class ServiceGrpc final: public PrimaryElection::Service {
+      public:
+        ServiceGrpc(CandidateDB* canddiate_db);
+        virtual ~ServiceGrpc();
+
+      public:
+        virtual grpc::Status updateAction(grpc::ServerContext* context, const Action* request, Action* response);
+        virtual grpc::Status updateItem(grpc::ServerContext* context, const Item* request, proto::Status* response);
+        virtual grpc::Status updateSubscribe(grpc::ServerContext* context, const Subscribe* request, proto::Status* response);
+        virtual grpc::Status updateFeedback(grpc::ServerContext* context, const Feedback* request, proto::Status* response);
+        virtual grpc::Status queryUserStatus(grpc::ServerContext* context, const User* request, UserStatus* response);
+        virtual grpc::Status queryCandidateSet(grpc::ServerContext* context, const Recommend* request, CandidateSet* response);
+        virtual grpc::Status queryUserInfo(grpc::ServerContext* context, const proto::UserQuery* request, proto::UserInfo* response);
+        virtual grpc::Status queryItemInfo(grpc::ServerContext* context, const proto::ItemQuery* request, proto::ItemInfo* response);
+
+      protected:
+        grpc::Status failed_status_glue(const Status& status);
+
+      private:
+        CandidateDB* candidate_db_;
+    };
+  } // namespace news
+} // namespace rsys
+#endif // #define RSYS_NEWS_SERVICE_GRPC_H
+
