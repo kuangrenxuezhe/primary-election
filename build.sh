51438a4bbb2452c8f9bf2040d00905a1fe01e136
shixz <shixz@zhongsou.com>
2016-01-06 12:12:49 +0800

    添加build脚本

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set +x
+DEBUG=""
+if [ "$#" -eq 1 ] && [ "$1"  == "debug" ]; then
+  DEBUG="debug=1"
+fi
+cd deps/src/utils
+git pull origin master
+make clean
+make $DEBUG
+make $DEBUG check
+make $DEBUG install prefix=../db/deps
+make $DEBUG install prefix=../..
+cd ../db
+git pull origin master
+make clean
+make $DEBUG
+make $DEBUG check
+make $DEBUG install prefix=../..
+cd ../../..
+git pull origin master
+make clean
+make $DEBUG
+make $DEBUG check
+
